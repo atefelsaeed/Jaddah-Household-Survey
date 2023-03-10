ec1e16022433625f69b1bf6569465dbafd74797d
AmrNassermohamed <48363120+AmrNassermohamed@users.noreply.github.com>
2023-03-10 21:44:31 +0200

    stable version without copy person trip

--- lib/UI/Screens/person/person_screen.dart
+++ lib/UI/Screens/person/person_screen.dart
@@ -415,16 +415,7 @@ class _PersonScreenState extends State<PersonScreen> {
                                                     debugPrint(p.toString());
                                                   },
                                                 ),
-                                      PersonModelList.personModelList[i]
-                                                  .occupationModel!.isEmployee ==
-                                              ""
-                                          ? Container()
-                                          : PersonModelList
-                                                      .personModelList[i]
-                                                      .occupationModel!
-                                                      .isEmployee ==
-                                                  "1"
-                                              ? PersonModelList.personModelList[i].personalQuestion!.mainOccupationType == "عاطلين عن العمل" ||
+                                      PersonModelList.personModelList[i].personalQuestion!.mainOccupationType == "عاطلين عن العمل" ||
 
                                                       PersonModelList
                                                               .personModelList[
@@ -589,9 +580,15 @@ class _PersonScreenState extends State<PersonScreen> {
                                                             : Container(),
                                                       ],
                                                     )
-                                              : Container()
+
                                     ],
                                   ),
+
+
+
+
+
+
                                   PersonModelList.personModelList[i]
                                               .occupationModel!.isEmployee ==
                                           "1"
